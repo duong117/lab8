8088145c42b6696b161c68d90906888b22cb6757
duong117 <47869833+duong117@users.noreply.github.com>
2019-04-11 17:11:35 -0500

    Student form and student table

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="app">
     <NewStudentForm v-on:student-added="newStudentAdded"></NewStudentForm>
-    <StudentTable v-bind:students="students"> c-on:student-present="studentArrivedOrLeft></StudentTable>
+    <StudentTable
+            v-bind:students="students"
+            v-on:student-present="studentArrivedOrLeft"
+            v-on:delete-student="studentDeleted">
+    </StudentTable>
     <StudentMessage v-bind:message="message" v-bind:name="name"></StudentMessage>
   </div>
 </template>
@@ -36,6 +40,9 @@ export default {
     studentArrivedOrLeft(student){
       this.message=student.present?'Welcome,':'Goodbye,'
       this.name=student.name
+    },
+    studentDeleted(student){
+      this.students=this.students.filter(function (s) {return s!=student})
     }
   }
 }
--- src/components/StudentTable.vue
+++ src/components/StudentTable.vue
@@ -2,6 +2,11 @@
     <div>
     <div class="card student-list m-2 p-2">
             <h4 class="card-title">Students</h4>
+        <div class="edit-table-toggle form-check">
+            <input id="edit-table" type="checkbox" class="form-check-input" v-model="editTable">
+            <label for="edit-table" class="form-check-label"> Edit table?</label>
+        </div>
+
             <div id="student-table">
                 <table class="table">
                     <tr>
@@ -9,12 +14,14 @@
                         <th>StarID</th>
                         <th>Present?</th>
                     </tr>
+                    <StudentRow
+                            v-for="student in students" v-bind:key="student.name"
+                            v-bind:student="student"
+                            v-bind:edit="editTable"
+                            v-on:student-present="studentArrivedOrLeft"
+                            v-on:delete-student="studentDeleted">
 
-                    <tr v-for="student in students" v-bind:class="'present-'+student.present" >
-                        <td>{{student.name}}</td>
-                        <td>{{student.starID}}</td>
-                        <td> <input type="checkbox" v-model="student.present" v-on:change="checked(student)"></td>
-                    </tr>
+                    </StudentRow>
                 </table>
             </div>
         </div>
@@ -23,14 +30,19 @@
 
 
 <script>
+    import StudentRow from '@/components/StudentRow.vue'
    export default{
        name:'StudentTable',
+       components:{StudentRow},
        props:{
            students:Array
        },
        methods:{
-           checked(student){
+           studentArrivedOrLeft(student){
                this.$emit('student-present',student)
+           },
+           studentDeleted(student){
+               this.$emit('delete-student',student)
            }
        }
    }
